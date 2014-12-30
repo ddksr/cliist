e0a80f20e2252d2bd2e326bafbe6da65ffcd5b5c
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-12-30 18:48:17 +0100

    adding tasks ... be awere of orgmode

--- cliist.el
+++ cliist.el
@@ -14,6 +14,9 @@
      
 ;;;###autoload
 
+(require 'dash)
+(require 'org)
+
 (setq cliist/list-exec "cliist %s --format org"
 	  cliist/exec "cliist %s")
 
@@ -21,7 +24,7 @@
   (switch-to-buffer (get-buffer-create (format "*Cliist: %s*" title)))
   (erase-buffer)
   (org-mode)
-  (insert (shell-command-to-string (format cliist/list-exec cliist-command)))
+  (insert (shell-command-to-string (format cliist/list-exec qcliist-command)))
   (goto-line 0) (move-to-column 0)
   (org-cycle) (org-cycle))
 
@@ -65,5 +68,71 @@
 (defun cliist/run (command)
   (interactive "sCommand: ")
   (shell-command (format cliist/exec command)))
+
+
+(defun cliist/empty (val)
+  (if (stringp val)
+	  (= (length val) 0)
+	(not val)))
+
+(defun cliist/not-empty (val)
+  (not (cliist/empty val)))
+
+(defun cliist/convert-date (&rest dates)
+  (car (split-string (or (-first 'cliist/not-empty
+								 dates) " ")
+				" ")))
+
+(defun cliist/org-get-info-at-point ()
+  (list (nth 4 (org-heading-components))
+		(cliist/convert-date (org-entry-get (point) "SCHEDULED")
+							 (org-entry-get (point) "DEADLINE"))
+		(car (-filter
+					'cliist/not-empty
+					(split-string (or (nth 5 (org-heading-components))
+									  "")
+								  ":")))))
+
+(defun cliist/get-info-at-point ()
+  (if (eq major-mode 'org-mode)
+	  (cliist/org-get-info-at-point)
+	(list (buffer-substring (line-beginning-position) (line-end-position))
+		  nil
+		  nil)))
+
+(defun cliist/add-task-cont (content &optional date tag)
+  (message "%s | %s | %s" date content tag)
+  (let ((c-date (read-from-minibuffer "Date: " (or date "")))
+		(c-content (read-from-minibuffer "Content: "
+										 (if (cliist/not-empty tag)
+											 (format "#%s %s" tag content)
+										   (or content "")))))
+	(message "%s -a %s"
+			 (if (cliist/not-empty c-date)
+				 (format "-d %s" c-date)
+			   "")
+			 c-content)))
+
+(defun cliist/add-task (&optional content date tag)
+  (interactive)
+  (apply 'cliist/add-task-cont
+		 (--zip-with (-first 'cliist/not-empty
+							 (list it other))
+					 (list content date tag)
+					 (cliist/get-info-at-point))))
+
+(define-minor-mode cliist-mode
+  "Communicate with cliist via Emacs"
+  :lighter " cliist"
+  :global t
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-c c r") 'cliist/run)
+			(define-key map (kbd "C-c c t") 'cliist/today-and-overdue)
+			(define-key map (kbd "C-c c c") 'cliist/completed)
+			(define-key map (kbd "C-c c p") 'cliist/project)
+			(define-key map (kbd "C-c c q") 'cliist/query)
+			(define-key map (kbd "C-c c v") 'cliist/view-all)
+			(define-key map (kbd "C-c c a") 'cliist/add-task)
+            map))
 
 (provide 'cliist)
